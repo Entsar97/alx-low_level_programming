09db87475b8610e982298addf31ceb2cf947f9b1
Entsar97 <entsarkambal5@gmail.com>
2023-09-18 11:26:33 -0700

    my commit

--- 0x05-pointers_arrays_string/0-reset_to_98.c
+++ 0x05-pointers_arrays_string/0-reset_to_98.c
@@ -1,10 +1,12 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
-  * reset_to_98 - Updates the value it points to 98
-  * @n: pointer to n
+  * reset_to_98 - Takes a pointer to an int and updates
+  * value it points to to 98
   *
-  * Return: void
+  * @n: An integer input pointer
+  *
+  * Return: nothing
   */
 void reset_to_98(int *n)
 {
