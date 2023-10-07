899c830cc5675d412ad86a1796520a92e351440b
Entsar97 <entsarkambal5@gmail.com>
2023-10-06 19:53:49 -0700

    tow

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,34 +11,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i;
-	int j;
-	unsigned int mem_al;
+	unsigned int x, y, z;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	if (n >= strlen(s2))
-		n = strlen(s2);
-	mem_al = strlen(s1) + n;
-	ptr = malloc(mem_al + 1);
-	if (ptr == NULL)
-		return (NULL);
-	j = 0;
-	for (i = 0; i < mem_al; i++)
 	{
-		if (i < strlen(s1))
-		{
-			ptr[i] = s1[i];
-		}
-		else
-		{
-			ptr[i] = s2[j];
-			j++;
-		}
+		x = 0;
 	}
-	ptr[i] = '\0';
-	return (ptr);
+	else
+	{
+		for (x = 0; s1[x]; ++x)
+		;
+	}
+	if (s2 == NULL)
+	{
+		y = 0;
+	}
+	else
+	{
+		for (y = 0; s2[y]; ++y)
+		;
+	}
+	if (y > n)
+		y = n;
+	s = malloc(sizeof(char) * (x + y + 1));
+	if (s == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
+	for (z = 0; z < y; z++)
+		s[z + x] = s2[z];
+	s[x + y] = '\0';
+	return (s);
 }
