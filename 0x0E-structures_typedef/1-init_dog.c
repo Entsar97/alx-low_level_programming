758198bd7cedfb5205e991b98b37c670360c8b8d
Entsar97 <entsarkambal5@gmail.com>
2023-10-10 12:11:35 -0700

    tow

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,24 +1,21 @@
 #include "dog.h"
-#include <stdio.h>
+
 /**
- * print_dog - prints a struct dog
- * @d: Pointer to struct
+ * init_dog - initializes a dog
+ * @d: the dog to init
+ * @name: the dog's name
+ * @age: dog's age
+ * @owner: owner's name
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void.
  */
-void print_dog(struct dog *d)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 {
-	if (d != 0)
+	if (d)
 	{
-		if ((*d).name != 0)
-			printf("Name: %s\n", (*d).name);
-		else
-			printf("Name: (nil)\n");
-		printf("Age: %f\n", (*d).age);
-		if ((*d).owner != 0)
-			printf("Owner: %s\n", (*d).owner);
-		else
-			printf("Owner: (nil)\n");
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+
 	}
 }
